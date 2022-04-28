9d4687e43200a7afd70f15b0f45cb5f818d9fd26
Yurino Oga <yurino.o@latona.io>
2022-04-28 15:54:44 +0900

    release

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	sap_api_caller "sap-api-integrations-real-estate-contract-reads/SAP_API_Caller"
-	"sap-api-integrations-real-estate-contract-reads/sap_api_input_reader"
+	sap_api_input_reader "sap-api-integrations-real-estate-contract-reads/SAP_API_Input_Reader"
 
 	"github.com/latonaio/golang-logging-library/logger"
 )
